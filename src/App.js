4fb426a90276254060583e636cf99e76b81f3b1d
rgeorge8 <rachel.george.uk@outlook.com>
2025-01-12 12:51:47 +0000

    add individual pages for items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import "./App.css";
 import HomePage from "./home";
-
+import ItemDetails from "./ItemDetails";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -25,36 +26,50 @@ const App = () => {
   };
 
   return (
-    
-    <div className="App">
-      {!isLoggedIn ? (
-        <div className="login-container">
-          <h1>Login</h1>
-          <div className="input-group">
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+    <Router>
+      <div className="App">
+        <Routes>
+          {!isLoggedIn ? (
+            <Route
+              path="/"
+              element={
+                <div className="login-container">
+                  <h1>Login</h1>
+                  <div className="input-group">
+                    <label htmlFor="username">Username:</label>
+                    <input
+                      type="text"
+                      id="username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </div>
+                  <div className="input-group">
+                    <label htmlFor="password">Password:</label>
+                    <input
+                      type="password"
+                      id="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                  {errorMessage && <p className="error">{errorMessage}</p>}
+                  <button onClick={handleLogin}>Login</button>
+                </div>
+              }
             />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          {errorMessage && <p className="error">{errorMessage}</p>}
-          <button onClick={handleLogin}>Login</button>
-        </div>
-      ) : (
-        <HomePage onLogout={handleLogout} />
-      )}
-    </div>
+          ) : (
+            <>
+              <Route
+                path="/"
+                element={<HomePage onLogout={handleLogout} />}
+              />
+              <Route path="/item/:id" element={<ItemDetails />} />
+            </>
+          )}
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
