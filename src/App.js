a70fcb1a126ede23229dd0132ea578f202a31eeb
rgeorge8 <rachel.george.uk@outlook.com>
2025-01-13 18:25:50 +0000

    remove unused code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // src/App.js
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./home";
 import AddItemPage from "./AddItemPage";
@@ -8,15 +8,6 @@ import LoginPage from "./LoginPage";
 import "./App.css";
 
 const App = () => {
-  // const [inventoryData, setInventoryData] = useState([]);
-
-  // // Load inventory data from localStorage when the app loads
-  // useEffect(() => {
-  //   const storedData = localStorage.getItem('inventoryData');
-  //   if (storedData) {
-  //     setInventoryData(JSON.parse(storedData));
-  //   }
-  // }, []);
 
   const [inventoryData, setInventoryData] = useState(() => {
     const storedData = localStorage.getItem("inventoryData");
